bbdc4e023143a869ff32710b73706c5ed50a473c
v_jianjpeng <v_jianjpeng@wesure.cn>
2021-04-12 08:24:31 +0800

    Initial Commit

---
+++ c++new/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++new
+//
+//  Created by 彭涧 on 2021/4/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
